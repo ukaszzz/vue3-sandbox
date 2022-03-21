24fe85e471e9e0e2fe7c436b29eb1d8367ec4ce7
Lukasz Zola <zolalukas@gmail.com>
2022-03-21 19:36:15 +0100

    watch

--- src/components/CustomComponent.vue
+++ src/components/CustomComponent.vue
@@ -1,32 +1,46 @@
 <template>
-<h1>{{price}}</h1>
-  <button @click="addOrder">add order</button>
-  <p>quantity: {{quantity}}</p>
-  <p>price: {{price}}</p>
-  <p>totPrice: {{totPrice}}</p>
-  <p>tax: {{tax}}</p>
+<div>
+  <p>shares: {{shares}}</p>
+  <p>price: {{sharesPrice}}</p>
+  <p>total: {{sharesTotal}}</p>
+  <button @click="changeSharesNumber(1)">buy 1</button>
+  <button @click="changeSharesNumber(5)">buy 5</button>
+  <button @click="changeSharesNumber(-1)">sell 1</button>
+  <button @click="changeSharesNumber(-5)">sell 5</button>
+</div>
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
   name: 'CustomComponent',
   setup() {
-    const quantity = ref(0);
-    const price = ref(100);
-    const totPrice = computed(() => price.value * quantity.value);
-    const tax = computed(() => totPrice.value * 0.23);
+    const shares = ref(15);
+    const sharesPrice = ref(20);
+    const sharesTotal = computed(() => shares.value * sharesPrice.value);
 
-    function addOrder() {
-      quantity.value += 1;
+    function changeSharesNumber(number) {
+      shares.value += number;
     }
+    function changePrice(min, max) {
+      const priceDiff = Math.floor(Math.random() * (max - min) + min);
+      console.log(priceDiff);
+      sharesPrice.value += priceDiff;
+      console.log(sharesPrice.value);
+    }
+
+    watch(shares, (prevSharesNumber, nextSharesNumber) => {
+      const newPrice = prevSharesNumber > nextSharesNumber
+        ? changePrice(1, 5) : changePrice(-1, -5);
+      console.log(newPrice);
+    });
+
     return {
-      addOrder,
-      quantity,
-      price,
-      totPrice,
-      tax,
+      shares,
+      sharesPrice,
+      sharesTotal,
+      changeSharesNumber,
     };
   },
 
