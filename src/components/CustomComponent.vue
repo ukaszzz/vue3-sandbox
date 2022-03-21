985a83fcaea575234d5339e02c815d2564ebf301
Lukasz Zola <zolalukas@gmail.com>
2022-03-21 19:08:32 +0100

    reactive

--- src/components/CustomComponent.vue
+++ src/components/CustomComponent.vue
@@ -1,22 +1,34 @@
 <template>
-<p>{{pension}}</p>
-  <button @click="pensionIncrement">increment</button>
+<h1>{{price}}</h1>
+  <button @click="addOrder">add order</button>
+  <p>quantity: {{quantity}}</p>
+  <p>price: {{price}}</p>
+  <p>totPrice: {{totPrice}}</p>
+  <p>tax: {{tax}}</p>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, toRefs } from 'vue';
 
 export default {
-  name: 'HelloWorld',
+  name: 'CustomComponent',
   setup() {
-    const pension = ref(100);
+    const state = reactive({
+      quantity: 0,
+      price: 100,
+      totPrice: 0,
+      tax: 0,
+    });
 
-    function pensionIncrement() {
-      pension.value += 1;
+    function addOrder() {
+      state.quantity += 1;
+      state.totPrice = state.quantity * state.price;
+      state.tax = state.totPrice * 0.23;
     }
+
     return {
-      pension,
-      pensionIncrement,
+      addOrder,
+      ...toRefs(state),
     };
   },
 
