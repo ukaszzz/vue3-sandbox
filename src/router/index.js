c21846287d30e74d2906308892fd019b7bfa1e46
Lukasz Zola <zolalukas@gmail.com>
2022-03-23 12:44:41 +0100

    page not found

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ItemView from '../views/ItemView.vue';
+import PageNotFound from '../views/PageNotFound.vue';
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Item',
     component: ItemView,
     props: true,
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'notFound',
+    component: PageNotFound,
   },
 ];
 
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h2>page not found</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+};
+</script>
